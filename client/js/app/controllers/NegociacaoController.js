5175edd4dbdca59f37f52a51ae64db7e077ef71c
Peterson Segatto Müller <p.smuller@outlook.com>
2020-04-23 17:05:24 -0300

    Métodos estáticos e expressões regulares

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,6 +7,7 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
 
     }
@@ -14,21 +15,30 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map((item, indice)=> item - indice % 2)
-            );
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-            let negociacao = new Negociacao(
-                data,
-                this._inputQuantidade.value,
-                this._inputValor.value
-            );
-       
-           console.log(negociacao);
-            
+        console.log(this._listaNegociacoes.negociacoes);   
 
         // adicionar a negociação em uma lista
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+
+    _limpaFormulario() {
+        
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
+
     }
 }
